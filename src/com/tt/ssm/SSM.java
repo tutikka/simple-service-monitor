f0bbb033c018cad7c2b1aa5907fd757667dd573c
Tuomas Tikka <tuomas.tikka@gmail.com>
2016-11-21 22:04:00 +0200

    use system look and feel by default

--- src/com/tt/ssm/SSM.java
+++ src/com/tt/ssm/SSM.java
@@ -1,23 +1,30 @@
 package com.tt.ssm;
 
 import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
+import com.tt.ssm.misc.Logger;
 import com.tt.ssm.view.SSMFrame;
 
 public class SSM {
 	
+	private static Logger logger = Logger.getLogger(SSM.class);
+	
 	public static void main(String[] args) {
+		logger.i("main");
 		Runnable runnable = new Runnable() {
 			@Override
 			public void run() {
 				try {
-					// UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 				} catch (Exception e) {
+					logger.w("error setting laf: " + e.getMessage());
 				}
 				new SSMFrame();
 			}
 		};
 		SwingUtilities.invokeLater(runnable);
+		logger.i("main completed");
 	}
 	
 }
