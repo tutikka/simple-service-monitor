94dd90675dbe9ebb24f8c21501d9faed0f9b31b9
Tuomas Tikka <tuomas.tikka@bcbmedical.com>
2016-11-30 09:09:42 +0200

    read look and feel from system property

--- src/com/tt/ssm/SSM.java
+++ src/com/tt/ssm/SSM.java
@@ -2,6 +2,7 @@ package com.tt.ssm;
 
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
 
 import com.tt.ssm.misc.Logger;
 import com.tt.ssm.view.SSMFrame;
@@ -12,12 +13,29 @@ public class SSM {
 	
 	public static void main(String[] args) {
 		logger.i("main");
-		System.setProperty("apple.laf.useScreenMenuBar", "true");
 		Runnable runnable = new Runnable() {
 			@Override
 			public void run() {
+				// laf
 				try {
-					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+					for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+						logger.i("found installed laf: " + info.getClassName());
+					}
+					//
+					// Examples from Ubuntu Linux with Java 7
+					//
+					// javax.swing.plaf.metal.MetalLookAndFeel
+					// javax.swing.plaf.nimbus.NimbusLookAndFeel
+					// com.sun.java.swing.plaf.motif.MotifLookAndFeel
+					// com.sun.java.swing.plaf.gtk.GTKLookAndFeel
+					//
+					String laf = System.getProperty("ssm.laf.class");
+					if (laf != null) {
+						logger.i("setting laf: " + laf);
+						UIManager.setLookAndFeel(laf);
+					} else {
+						logger.i("using default laf");
+					}
 				} catch (Exception e) {
 					logger.w("error setting laf: " + e.getMessage());
 				}
